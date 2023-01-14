636058b4ca0b0208db9ea0cc1bb0eaa87b8e0475
‘xiuxiu-yang’ <1974364190@qq.com>
2023-01-14 18:29:12 +0800

    fix: InjectMometaSFC修复

--- packages/app/vite.config.ts
+++ packages/app/vite.config.ts
@@ -1,7 +1,10 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 
+//@ts-ignore
+import MometaVue from '../editor/vite'
+
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue()],
+  plugins: [MometaVue(), vue()],
 })
--- packages/editor/vite/InjectMometaSFC.ts
+++ packages/editor/vite/InjectMometaSFC.ts
@@ -17,31 +17,31 @@ const toString = (obj: any) => {
 
 const InjectMometaSFC = (code: string): string => {
   const descriptor = parse(code).descriptor
-  const template = descriptor.template?.content.split('\n')!
+  const template = code.split('\n')!
   const ast = descriptor.template?.ast!
 
   const DFS = (node) => {
-    if (node.type === 1) {
+    if (node.type === 1 && !node.tagType) {
       const children = node.children
       for (let i = children.length - 1; i >= 0; i--) {
         DFS(children[i])
       }
+
+      const start = node.loc.start
+      const end = node.loc.end
+
+      const mometa = {
+        start,
+        end,
+      }
+
+      const sliceBegain = start.column + node.tag.length
+      const line = template[start.line - 1]
+      template[start.line - 1] =
+        line.slice(0, sliceBegain) +
+        ` :mometa="${toString(mometa)}"` +
+        line.slice(sliceBegain)
     }
-
-    const start = node.loc.start
-    const end = node.loc.end
-
-    const mometa = {
-      start,
-      end,
-    }
-
-    const sliceBegain = start.column + node.tag.length
-    const line = template[start.line - 1]
-    template[start.line - 1] =
-      line.slice(0, sliceBegain) +
-      ` :mometa="${toString(mometa)}"` +
-      line.slice(sliceBegain)
   }
 
   for (let i = ast.children.length - 1; i >= 0; i--) {
